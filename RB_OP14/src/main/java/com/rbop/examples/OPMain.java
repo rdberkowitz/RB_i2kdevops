a5f13658204ae1f7ce35a2d59ea4c625aed230b9
rdberkowitz <rdberkowitz@gmail.com>
2019-10-19 12:21:37 -0700

    Output file path and run 100 rows

--- RB_OP14/src/main/java/com/rbop/examples/OPMain.java
+++ RB_OP14/src/main/java/com/rbop/examples/OPMain.java
@@ -16,7 +16,7 @@ public class OPMain {
         try {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/i2kdocs","root","Qw58v6A8");
-            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM alist_geography WHERE id > 0 LIMIT 500");
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM alist_geography WHERE id > 0 LIMIT 100");
             /* Connection conn = DriverManager.getConnection("jdbc:h2:file://Users/rachelberkowitz/i2k/OPRB_rb3");
             ResultSet rs = conn.createStatement().executeQuery("SELECT TOP 28 * FROM ALIST_GEOGRAPHY_rb3");
             Connection conn = DriverManager.getConnection("jdbc:h2:file://Users/rachelberkowitz/i2k/OPRB_all_rb4");
